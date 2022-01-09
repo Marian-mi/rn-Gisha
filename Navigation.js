afe9273d51446e74fc345e0514418f6c47d3df57
mahdi <mahdi.maryan@yahoo.com>
2022-01-09 09:43:55 +0330

    SYNC

--- Navigation.js
+++ Navigation.js
@@ -1,9 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React, { useState } from 'react'
-import { View, Text, StyleSheet, Pressable } from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Colors } from './src/Styles/Index';
 
 //#region Pages
@@ -19,141 +19,100 @@ const Stack = createNativeStackNavigator();
 const NavigationStacks = () => {
     return (
         <NavigationContainer>
-            <Drawer.Navigator
-                drawerContent={(props) => <CustomDrawerContent {...props} />}
-            >
+            <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
                 <Drawer.Screen
-                    name='Home'
-                    component={ScreenStack({ screen: Home, name: "HomeStack" })}
+                    name="Drawer"
+                    component={ScreenStack}
                     options={{
-                        ...DrawerCommonStyles,
-                    }}
-                />
-                <Drawer.Screen
-                    name="ProductCategory"
-                    component={ScreenStack({ screen: ProductCategory, name: "ProductCategoryStack" })}
-                    options={{
-                        ...DrawerCommonStyles,
-                    }}
-                />
-                <Drawer.Screen
-                    name="ProductGroup"
-                    component={ScreenStack({ screen: ProductGroup, name: "ProductGroupStack" })}
-                    options={{
-                        ...DrawerCommonStyles,
-                    }}
-                />
-                <Drawer.Screen
-                    name="Search"
-                    component={ScreenStack({ screen: Search, name: "SearchStack" })}
-                    options={{
-                        ...DrawerCommonStyles,
+                        drawerStyle: { top: -5 },
+                        headerShown: false,
                     }}
                 />
             </Drawer.Navigator>
-
         </NavigationContainer>
-    )
-}
+    );
+};
 
-const DrawerCommonStyles = {
-    drawerStyle: { top: -5 },
-    headerShown: false
-}
+const StackOptions = {
+    headerShown: false,
+};
 
-
-const ScreenStack = ({ screen, name }) => {
-    const ScreenHC = () => (
+const ScreenStack = () => {
+    return (
         <Stack.Navigator>
-            <Stack.Screen
-                name={name}
-                component={screen}
-                options={{
-                    headerShown: false,
-                }}
-            />
+            <Stack.Screen name="Home" component={Home} options={StackOptions} />
+            <Stack.Screen name="ProductCategory" component={ProductCategory} options={StackOptions} />
+            <Stack.Screen name="ProductGroup" component={ProductGroup} options={StackOptions} />
+            <Stack.Screen name="Search" component={Search} options={StackOptions} />
         </Stack.Navigator>
-    )
+    );
+};
 
-    return ScreenHC
-}
-
-const CustomDrawerContent = (props) => {
-    const routes = props.state.routes.map((route) => {
-        const { name } = route;
-        const { Label, Icon, Hidden } = RoutesConfig[name];
-
-        if (Hidden)
-            return null
-
-        return (
-            <DrawerItem
-                label={Label ?? name}
-                onPress={() => props.navigation.navigate(name)}
-                icon={Icon ?? (() => { })}
-            />
-        );
-    })
-
+const CustomDrawerContent = props => {
     return (
         <DrawerContentScrollView {...props}>
             <DrawerHeader />
-            {routes}
+            <DrawerItem
+                label={() => <Text>خانه</Text>}
+                onPress={() => props.navigation.navigate('Home')}
+                icon={() => <Icon name="home" size={26} color="#5f5f5f" />}
+            />
+            <DrawerItem
+                label={() => <Text>لیست دسته بندی محصولات</Text>}
+                onPress={() => props.navigation.navigate('ProductCategory')}
+                icon={() => (
+                    <Icon
+                        name="format-list-bulleted"
+                        size={26}
+                        color="#5f5f5f"
+                        style={{
+                            transform: [
+                                {
+                                    rotate: '180deg',
+                                },
+                            ],
+                        }}
+                    />
+                )}
+            />
         </DrawerContentScrollView>
-    )
-}
-
-const RoutesConfig = {
-    "Home": {
-        Label: "خانه",
-        Icon: () => <Icon name='home' size={26} color="#5f5f5f" />,
-    },
-    "ProductCategory": {
-        Label: "لیست دسته بندی محصولات",
-        Icon: () => <Icon name='format-list-bulleted' size={26} color="#5f5f5f" style={{ transform: [{ rotate: "180deg" }] }} />
-    },
-    "ProductGroup": {
-        Hidden: false,
-    },
-    "Search": {
-        Hidden: true,
-    }
-}
+    );
+};
 
 const DrawerHeader = () => {
     return (
         <View style={DrawerStyles.HeaderContainer}>
-            <Icon name='account' color="white" size={26} style={{ marginEnd: 15 }} />
+            <Icon name="account" color="white" size={26} style={{ marginEnd: 15 }} />
             <Text style={DrawerStyles.HeaderText}>ورود و ثبت نام</Text>
         </View>
-    )
-}
+    );
+};
 
 const DrawerStyles = StyleSheet.create({
     Label: {
         fontSize: 15,
         color: '#5f5f5f',
-        fontWeight: "700"
+        fontWeight: '700',
     },
     HeaderContainer: {
         height: 80,
-        display: "flex",
+        display: 'flex',
         justifyContent: 'flex-start',
         alignItems: 'center',
         flexDirection: 'row',
         paddingStart: 15,
-        backgroundColor: Colors.Primary
+        backgroundColor: Colors.Primary,
     },
     HeaderText: {
         paddingHorizontal: 10,
         paddingVertical: 8,
-        fontWeight: "700",
+        fontWeight: '700',
         borderRadius: 8,
         borderColor: 'white',
         color: 'white',
         borderWidth: 1.5,
-        textAlign: 'center'
-    }
-})
+        textAlign: 'center',
+    },
+});
 
-export default NavigationStacks
+export default NavigationStacks;
